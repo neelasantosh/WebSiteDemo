47db08d8cdb6564182474f580b19dfb84c36de7a
neelasantosh <neela.santosh2405@gmail.com>
2016-07-31 16:50:44 +0530

    Demo

---
+++ App_Code/WebService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+/// <summary>
+/// Summary description for WebService
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+// [System.Web.Script.Services.ScriptService]
+public class WebService : System.Web.Services.WebService {
+
+    public WebService () {
+
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+
+
+    //This is called metadata in java it is called annotation
+    [WebMethod]
+    public bool Login(string userId, string password)
+    {
+        if (userId == "mohsin" && password == "123") return true;
+        else return false;
+    }
+    
+}
